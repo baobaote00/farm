896d3fe8b5ef21d2970b6dc826bcfed48928c695
baobaote00 <baobaote00@gmail.com>
2021-08-30 13:07:39 +0700

    :heart:

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
 import background from './img/bg.png';
+import ruong from './img/ruong.png';
+import bap from './img/bap.jfif';
 import './app.css'
 
 function App() {
   return (
     <div className="App">
       <img src={background} className="App-logo" alt="logo" />
+      <div className="ruong">
+        <img src={ruong} className="ruong-chil" alt="logo" />
+        <img src={ruong} className="ruong-chil" alt="logo" />
+        <img src={ruong} className="ruong-chil" alt="logo" />
+        <img src={ruong} className="ruong-chil" alt="logo" />
+        <img src={ruong} className="ruong-chil" alt="logo" />
+        <img src={ruong} className="ruong-chil" alt="logo" />
+        <div className="cay">
+          <div className="day-ruong">
+            <div className="o-ruong"><img src={bap} className="cai-cay" alt="logo" /></div>
+            <div className="o-ruong"></div>
+            <div className="o-ruong"></div>
+            <div className="o-ruong"></div>
+            <div className="o-ruong"></div>
+            <div className="o-ruong"></div>
+          </div>
+          <div className="day-ruong">
+            <div className="o-ruong"><img src={bap} className="cai-cay" alt="logo" /></div>
+            <div className="o-ruong"></div>
+            <div className="o-ruong"></div>
+            <div className="o-ruong"></div>
+            <div className="o-ruong"></div>
+            <div className="o-ruong"></div>
+          </div>
+          <div className="day-ruong">
+            <div className="o-ruong"></div>
+            <div className="o-ruong"><img src={bap} className="cai-cay" alt="logo" /></div>
+            <div className="o-ruong"></div>
+            <div className="o-ruong"></div>
+            <div className="o-ruong"></div>
+            <div className="o-ruong"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
